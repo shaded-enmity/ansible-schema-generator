6d6a90bd4355477225aa449df6600bae55d02ef2
Pavel Odvody <podvody@redhat.com>
2018-09-08 01:55:08 +0200

    Remove old test code

--- test.py
+++
@@ -1,9 +0,0 @@
-import jsonschema, json, os, yaml
-
-schema = json.loads(open("generated/ansible-stable-2.5.json").read())
-cases = os.listdir('test/cases')
-numcases = len(cases)
-
-for (n, f) in enumerate(cases):
-    print('[{n}/{total}] Validating: {file}'.format(file=f, n=n+1, total=numcases))
-    jsonschema.validate(list(yaml.load_all(open('test/cases/' + f).read())), schema)
