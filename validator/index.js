0127996b5fbdcc54ba4c234fa6a9e237e7baa049
Pavel Odvody <podvody@redhat.com>
2018-09-08 01:47:21 +0200

    Fix test case validation

--- validator/index.js
+++ validator/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const tv4 = require('tv4');
+const yaml = require('js-yaml');
 
 let schema = JSON.parse(
   fs.readFileSync('../generated/ansible-stable-2.5.json')
@@ -8,14 +9,14 @@ let schema = JSON.parse(
 var counter = 0;
 fs.readdir('../test/cases/', (err, files) => {
   files.forEach(file => {
-    const testCase = JSON.parse(
-      fs.readFileSync('../generated/ansible-stable-2.5.json')
+    const testCase = yaml.loadAll(
+      fs.readFileSync('../test/cases/' + file)
     );
 
-    const validation = tv4.validate(testCase, schema);
-    if (validation.error !== undefined) {
-      console.log(validation)
-      throw validation
+    const valid = tv4.validate(testCase, schema);
+    if (!valid) {
+      console.log(file, tv4.error)
+      throw tv4.error
     }
     
     console.log('['+(counter+1)+'/'+files.length+'] Validating: '+ file);
