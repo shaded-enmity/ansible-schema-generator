7c7d437af3d76ee9667b5594cfcaac387a64465d
Pavel Odvody <podvody@redhat.com>
2018-09-07 18:38:19 +0200

    Balance parenthesis in test script

--- test.sh
+++ test.sh
@@ -12,5 +12,5 @@ cd -
 ANSIBLE_DIR=ansible/ ARGSPEC_PATH=ansible-stable/test/sanity/validate-modules/module_args.py ./generate.sh
 
 for TC in test/cases/*; do
-  python3 <<< "import jsonschema, json, yaml; jsonschema.validate(yaml.load(open(\"${TC}\").read()), json.loads(open(\"generated/ansible-stable-2.5.json\").read())"
+  python3 <<< "import jsonschema, json, yaml; jsonschema.validate(yaml.load(open(\"$TC\").read()), json.loads(open(\"generated/ansible-stable-2.5.json\").read()))"
 done
